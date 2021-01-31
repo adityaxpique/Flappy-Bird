53cc062437de40071ed6f5647431ea026636f0d3
aditya <aditya29ks@gmail.com>
2021-01-31 16:01:25 -0500

    Flappy Bird game

---
+++ Assets/AddScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour
+{
+
+    public int highScore = 0;
+    public Text highscoretext;
+    public Text scoreText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        UpdateScore();
+    }
+
+    public void UpdateScore()
+    {
+        Score.score = Score.score + 1;
+        scoreText.text = "S:" + Score.score;
+
+        int highScore = PlayerPrefs.GetInt("HIGHSCORE");
+        if (Score.score > highScore)
+        {
+            PlayerPrefs.SetInt("HIGHSCORE", Score.score);
+
+            highscoretext.text = "HS:" + Score.score;
+        }
+        else
+        {
+            highscoretext.text = "HS:" + highScore;
+        }
+
+
+
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+
+public class Score : MonoBehaviour
+{
+    public static int score = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+        
+
+        
+    }
+}
